3093c96c5eb0b13220666cd0e86e573681a50ff6
richardliuu <richardliu200127@gmail.com>
2025-02-08 23:18:33 -0500

    changes to the backend

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -9,22 +9,37 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group = f'chat_P{self.room_name}'
 
-        await self.channel_layer.group_add(self.room_name, self.channel_name)
+        await self.channel_layer.group_add(self.room_group, self.channel_name)
         await self.accept()
         
-        async def disconnect(self, close_code):
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group, self.channel_name)
 
-            await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+    async def receive(self, text_data):
+        await self.channel_layer.group_send(
+            self.room_group, 
+            {
+                'type': 'chat_message',
+                'message': text_data
+            }
 
-        async def receive(self, text_data):
-            await self.channel_layer.group_send(self.room_group_name, self.channel_name
+        )
 
-            )
+    async def chat_message(self, event):
+        message = event['message']
+
+        await self.send(text_data=json.dumps({
+            'message': message 
+        }))
 
 # Notifications
 
 class Notification(AsyncWebsocketConsumer):
     async def connect(self):
+        self.user_ID = self.scope['self'].id
+        self.room_group_name = f'notifications{self.user_ID}'        
+        
+        await self.channel_layer.group_add(self.room_group_name, self.channel_layer) 
         await self.accept()
 
 
@@ -37,3 +52,10 @@ class Notification(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({'message': message}))
 
     
+
+    async def notification_send(self, event):
+        message = event['message']
+
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
