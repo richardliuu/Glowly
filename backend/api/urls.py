80d91221160cc1a51c89fd6d893d3fd818949771
richardliuu <richardliu200127@gmail.com>
2025-02-10 17:42:04 -0500

    started a folder for views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+# When creating the views folder, will need to change how the class based views are imported 
+
 urlpatterns = [
     path("posts/", views.PostListCreate.as_view(), name="post-list"),
     path("posts/delete/<int:pk>/", views.PostDelete.as_view(), name="post-delete")
