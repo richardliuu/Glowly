22d872549ea862cc9f6b36ff7f96db47d8f0c0b9
richardliuu <richardliu200127@gmail.com>
2025-02-11 22:04:06 -0500

    organized views folder

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
-from api.views
+from api.views import PostListCreate, PostDelete, PostEdit, PostLike
+from api.views import CommentDelete, CommentLike, CommentListCreate
+from api.views import CreateUserView
 
 # When creating the views folder, will need to change how the class based views are imported 
 
 urlpatterns = [
-    path("posts/", views.PostListCreate.as_view(), name="post-list"),
-    path("posts/delete/<int:pk>/", views.PostDelete.as_view(), name="post-delete")
-]
+    path("posts/", PostListCreate.as_view(), name="post-list"),
+    path("posts/delete/<int:pk>/", PostDelete.as_view(), name="post-delete"),
+    path(),
+    path(), 
+    path(),
+    path(),
+]    
---
+++ backend/api/views/__init__.py
@@ -0,0 +1,3 @@
+from .post_views import PostListCreate, PostLike, PostDelete, PostEdit
+from .user_views import CreateUserView
+from comment_views import CommentDelete, CommentLike, CommentListCreate
