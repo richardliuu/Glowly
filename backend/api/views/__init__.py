9b9232e98920c3114160e3457c5edb723100ab5e
richardliuu <richardliu200127@gmail.com>
2025-02-12 21:58:43 -0500

    migrated models

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -1,3 +1,3 @@
 from .post_views import PostListCreate, PostLike, PostDelete, PostEdit
 from .user_views import CreateUserView
-from comment_views import CommentDelete, CommentLike, CommentListCreate
+from .comment_views import CommentDelete, CommentLike, CommentListCreate
--- backend/api/views/post_views/post_edit.py
+++ backend/api/views/post_views/post_edit.py
@@ -6,9 +6,9 @@ from api.serializers import PostSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from api.models import Post
 
-class PostEdit():
+class PostEdit(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        pass
+        return Post.objects.filter(author=self.request.user)
