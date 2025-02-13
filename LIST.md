13ceb820c12692e3e225fda5c0f953c882ece750
richardliuu <richardliu200127@gmail.com>
2025-02-13 10:52:23 -0500

    registered post model in admin

---
+++ LIST.md
@@ -0,0 +1,7 @@
+# Running the backend
+
+cd backend
+python manage.py runserver
+
+# Need to access the REST framework to check if the models can correctly input data 
+
--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from api.models import Post, Comment, Profile, Like
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'text_preview', 'created_date', 'image_preview', 'likes_count')
+    search_fields = ('author__username', 'text')
+    list_filter = ('created_date',)
+    readonly_fields = ('created_date', 'image_preview')
+    ordering = ('-created_date',)
+    fields = ('author', 'text', 'image', 'image_preview', 'created_date', 'liked_by')
+
+    def text_preview(self, obj):
+        return obj.text[:50] + "..." if len(obj.text) > 50 else obj.text
+    text_preview.short_description = "Text Preview"
+
+    def image_preview(self, obj):
+        if obj.image:
+            return f'<img src="{obj.image.url}" width="100" height="100" style="border-radius:5px;"/>'
+        return "No Image"
+    image_preview.allow_tags = True
+    image_preview.short_description = "Image Preview"
+
+    def likes_count(self, obj):
+        return obj.liked_by.count()
+    likes_count.short_description = "Likes"
+
