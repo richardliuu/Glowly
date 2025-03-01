b1dd68ac7115c362216eaece59dace9167182ae0
Crackle2K <crackleontwitch@gmail.com>
2025-03-01 14:39:22 -0500

    Updated models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -70,3 +70,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class MentalHealthResource(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    link = models.URLField()
+
+    def __str__(self):
+        return self.name
